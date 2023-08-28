b4778134de73555de3172ef607bf53bc1e4a28fb
Nick <darkforceyoda@gmail.com>
2023-08-28 12:51:23 -0500

    Rightsidebar layout/styles

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,6 +3,14 @@ import React from "react";
 type RightSidebarProps = {};
 
 const RightSidebar: React.FC<RightSidebarProps> = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+    </section>
+  );
 };
 export default RightSidebar;
