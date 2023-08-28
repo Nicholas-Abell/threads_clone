b206c0bfea5ef9611444b5e47731c28082dcdb0b
Nick <darkforceyoda@gmail.com>
2023-08-28 11:05:44 -0500

    LeftSideNav

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,42 @@
+"use client";
 import React from "react";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 type LeftSidebarProps = {};
 
 const LeftSidebar: React.FC<LeftSidebarProps> = () => {
-  return <div>Leftsidebar</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={isActive ? "bg-primary-500" : "leftsidebar_link"}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 export default LeftSidebar;
