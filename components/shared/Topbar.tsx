e9ef15633f555a13d8554240386c705ddbb7acc1
Nick <darkforceyoda@gmail.com>
2023-08-28 10:43:21 -0500

    navbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
-  return <div>TopBar</div>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-sm:hidden">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image src="logout.svg" alt="logout" width={28} height={28} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: { organizationSwitcherTrigger: "py-2 px-4" },
+          }}
+        />
+      </div>
+    </nav>
+  );
 };
 export default Topbar;
