90b83ac391e8961483a4e8ede3799780c12ba761
Nick <darkforceyoda@gmail.com>
2023-08-29 10:50:46 -0500

    onbarding, AccountProfile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,26 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
   return (
-    <main>
-      <h1 className="head-text text-black">on boarding</h1>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">on boarding</h1>
+      <p>Complete your profile</p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle={"Push"} />
+      </section>
     </main>
   );
 }
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { usePathname, useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { UserValidation } from "@/lib/validations/user";
+
+type AccountProfileProps = {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+};
+
+const AccountProfile: React.FC<AccountProfileProps> = ({ user, btnTitle }) => {
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+export default AccountProfile;
